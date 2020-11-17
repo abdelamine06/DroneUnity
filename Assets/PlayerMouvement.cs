10700b4274014cd26d6ca2a1203270f324abdbd1
Amine <abdelamine.mehdaoui@gmail.com>
2020-11-17 01:19:14 +0100

    first commit

---
+++ Assets/PlayerMouvement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMouvement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(0,0,600); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(50,0,0);
+        }
+
+        if (Input.GetKey("g"))
+        {
+             rb.AddForce(-50,0,0);
+        }
+    }
+}
